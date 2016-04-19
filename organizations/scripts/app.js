30652efd8814402ef43e5541406eb035b35ec4dc
Paul D Barham <paulb@savagesoft.com>
2016-04-19 17:26:02 -0500

    Freash copy from https://github.com/shyamseshadri/angularjs-up-and-running.git

--- organizations/scripts/app.js
+++ organizations/scripts/app.js
@@ -1,19 +1,31 @@
 // File: chapter10/routing-example/app/scripts/app.js
 angular.module('fifaApp', ['ngRoute'])
-    .config(function ($routeProvider) {
+  .config(function($routeProvider) {
 
-        $routeProvider.when('/', {
-                templateUrl: 'views/team_list.html',
-                controller: 'TeamListCtrl as teamListCtrl'
-            })
-            .when('/login', {
-                templateUrl: 'views/login.html'
-            })
-            .when('/team/:code', {
-                templateUrl: 'views/team_details.html',
-                controller: 'TeamDetailsCtrl as teamDetailsCtrl'
-            });
-        $routeProvider.otherwise({
-            redirectTo: '/'
-        });
+    $routeProvider.when('/', {
+      templateUrl: 'views/team_list.html',
+      controller: 'TeamListCtrl as teamListCtrl'
+    })
+    .when('/login', {
+      templateUrl: 'views/login.html'
+    })
+    .when('/team/:code', {
+      templateUrl: 'views/team_details.html',
+      controller:'TeamDetailsCtrl as teamDetailsCtrl',
+      resolve: {
+        auth: ['$q', '$location', 'UserService',
+          function($q, $location, UserService) {
+             return UserService.session().then(
+               function(success) {},
+               function(err) {
+                  $location.path('/login');
+                  $location.replace();
+                  return $q.reject(err);
+             });
+        }]
+      }
     });
+    $routeProvider.otherwise({
+      redirectTo: '/'
+    });
+  });
--- organizations/scripts/services.js
+++ organizations/scripts/services.js
@@ -1,40 +1,36 @@
 // File: chapter10/routing-example/app/scripts/services.js
-
-// 1g6GIToW1Mhq83zVW4nJe4Y573v5MJOztqn3qvjwWX7s
-
 angular.module('fifaApp')
-    .factory('FifaService', ['$http',
-        function ($http) {
-            return {
-                getTeams: function () {
-                    return $http.get('/api/team');
-                },
+  .factory('FifaService', ['$http',
+    function($http) {
+      return {
+        getTeams: function() {
+          return $http.get('/api/team');
+        },
 
-                getTeamDetails: function (code) {
-                    return $http.get('/api/team/' + code);
-                }
-            }
-        }])
+        getTeamDetails: function(code) {
+          return $http.get('/api/team/' + code);
+        }
+      }
+  }])
+  .factory('UserService', ['$http', function($http) {
+    var service = {
+      isLoggedIn: false,
 
-    .factory('UserService', ['$http', function ($http) {
-        var service = {
-            isLoggedIn: false,
+      session: function() {
+        return $http.get('/api/session')
+              .then(function(response) {
+          service.isLoggedIn = true;
+          return response;
+        });
+      },
 
-            session: function () {
-                return $http.get('/api/session')
-                    .then(function (response) {
-                        service.isLoggedIn = true;
-                        return response;
-                    });
-            },
-
-            login: function (user) {
-                return $http.post('/api/login', user)
-                    .then(function (response) {
-                        service.isLoggedIn = true;
-                        return response;
-                    });
-            }
-        };
-        return service;
-    }]);
+      login: function(user) {
+        return $http.post('/api/login', user)
+          .then(function(response) {
+            service.isLoggedIn = true;
+            return response;
+        });
+      }
+    };
+    return service;
+  }]);
